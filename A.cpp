d135e95d123aca0d579da0119cc27881565503f4
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-07-21 22:54:07 +0600

    Initial commit

---
+++ A.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Jihad on 7/21/2022.
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+void solve()
+{
+    cout << "me noob";
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ll test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
