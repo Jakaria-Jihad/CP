116be16b75939c1dfcab0334009a028671a52b63
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-07-22 00:50:32 +0600

    Solved!! yayyyyy. Always remember that reverse means opposite. So, put the begin() and end() correctly.

---
+++ CodeWars/Reversed Strings.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Jihad on 7/21/2022.
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+string reverseString (string str )
+{
+    string b = str;
+    reverse(b.begin(), b.end());
+    return b;
+}
+void solve()
+{
+    cout << reverseString("hello");
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ll test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
