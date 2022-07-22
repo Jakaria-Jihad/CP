7a6384e41ffba3ee338f69b130ad85538c0abd67
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-07-22 23:36:54 +0600

    Solved!! yayyyyy. 'std' will kill you!

--- CodeWars/Square(n) Sum.cpp
+++ CodeWars/Square(n) Sum.cpp
@@ -1,3 +1,37 @@
 //
 // Created by Jihad on 7/22/2022.
 //
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+int square_sum(const vector<int> &numbers) {
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        sum += numbers.at(i) * numbers.at(i);
+    }
+    return sum;
+}
+
+void solve() {
+    cout << square_sum({1, 2});
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ll test = 1;
+//cin>>test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
