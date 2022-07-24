17981ece175a5f711b80871cd227e5d0a08acf32
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-07-24 22:34:28 +0600

    Solved!! yayyyyy. is this even a problem?

---
+++ CodeWars/You Can't Code Under Pressure #1.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Jihad on 7/24/2022.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+int32_t double_integer(int32_t n) {
+    // We need solution fast !!!
+    return 2 * n;
+}
+
+void solve() {
+    cout << double_integer(20);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ll test = 1;
+//cin>>test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
